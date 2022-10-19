0be39705e2f641363eba6ddf89eec34f5db95e66
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-10-19 07:54:56 -0300

    Adicionado algumas mascaras

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import "./css/index.css"
+import IMask from'imask';
 
-const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
-const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+const date = new Date();
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+
 
 
 
@@ -15,9 +18,41 @@ function setCardType(type) {
     default: ["black", "gray"],
   }
 
-  ccBgColor01.setAttribute("fill", colors[type][0])
-  ccBgColor02.setAttribute("fill", colors[type][1])
-  ccLogo.setAttribute("src", `cc-${type}.svg`)
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor02.setAttribute("fill", colors[type][1]);
+  ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
 
-globalThis.setCardType = setCardType
+globalThis.setCardType = setCardType;
+
+// Security Code
+const securityCode = document.querySelector("#security-code");
+const securityCodePattern = {
+  mask:"0000"
+}
+
+const securityCodeMask = IMask(securityCode,securityCodePattern)
+
+// Expiracao 
+var anoAtual = date.getFullYear();
+anoAtual = anoAtual.toString()[2] + anoAtual.toString()[3]
+anoAtual = parseInt(anoAtual)
+const expirationDate = document.querySelector("#expiration-date");
+const expirationDatePattern = {
+  mask:"MM{/}YY",
+  blocks: {
+    MM:{
+      mask:IMask.MaskedRange,
+      from: 1,
+      to:12
+    },
+    YY:{
+      mask:IMask.MaskedRange,
+      from:anoAtual,
+      to:anoAtual + 10
+
+
+    }
+  }
+}
+const expirationDateMasked = IMask(expirationDate,expirationDatePattern)
